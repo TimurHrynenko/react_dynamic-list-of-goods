de6e6619c32ee9b62166c55fbecd43178a2568ae
Timur <hrynenko.timur@gmail.com>
2021-06-01 11:47:47 +0300

    Updates

---
+++ precache-manifest.100168f62b7c9e05cd4eec3219dd659c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eef451d60bf97010e7e4ea6f17ea487",
+    "url": "./index.html"
+  },
+  {
+    "revision": "adcd4f7e8f8beed67010",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "569049a0f7a2898af4df",
+    "url": "./static/js/2.c53bfda9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c53bfda9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adcd4f7e8f8beed67010",
+    "url": "./static/js/main.933a5daa.chunk.js"
+  },
+  {
+    "revision": "cc2a5e26d5c4844758e0",
+    "url": "./static/js/runtime-main.beb1984a.js"
+  }
+]);
